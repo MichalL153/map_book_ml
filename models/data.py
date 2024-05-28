298d1b6b015009bfc32507b534cf04d20b8c60ab
MichalL153 <167193757+MichalL153@users.noreply.github.com>
2024-05-28 19:21:11 +0200

    wip: goi progres

--- models/data.py
+++ models/data.py
@@ -1,7 +1,8 @@
 users: list[dict[str, str]] = [
-    {'name': 'Michał', 'surname': 'Lębryk', 'post': 1},
-    {'name': 'Kacper', 'surname': 'Macioch', 'post': 2},
-    {'name': 'Michał', 'surname': 'Krzywiński', 'post': 3},
-    {'name': 'Tymon', 'surname': 'Leszczyc', 'post': 2},
-    {'name': 'Staś', 'surname': 'Grzymski', 'post': 4},
+    {'name': 'Staś', 'surname': 'Grzymski', 'post': 1, 'location':'Konin'},
+    {'name': 'Kacper', 'surname': 'Macioch', 'post': 2, 'location':'Ząbki'},
+    {'name': 'Michał', 'surname': 'Krzywiński', 'post': 3, 'location':'Brodnica'},
+    {'name': 'Tymon', 'surname': 'Leszczyc', 'post': 2, 'location':'Toruń'},
+    {'name': 'Michał', 'surname': 'Lębryk', 'post': 2, 'location':'Lublin'},
+
 ]
